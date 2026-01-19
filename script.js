895ea7696eba99b812b6d72acd1fcf2da8183709
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-01-19 22:17:40 +0300

    fix: fixed mobile responsiveness issues

--- script.js
+++ script.js
@@ -3,12 +3,18 @@
 const yearElement = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearElement.textContent = currentYear;
-
+    
 const btnNavEl = document.querySelector(".btn-mobile-nav"); 
 const headerEl = document.querySelector(".header");
+
+const alllinks = document.querySelectorAll(".header__nav-link");
+
+alllinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    headerEl.classList.remove("nav-open");
+  });
+});
 
 btnNavEl.addEventListener("click", () => {
     headerEl.classList.toggle("nav-open");
 });
-
-console.log("JavaScript is working!");
