84ed84b1d060b4a33eca6a534419e06e4c628d72
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-06-06 16:58:45 +0300

    feat: implement menu fade functionality

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+"use strict";
+
+const nav = document.querySelector(".header__nav");
+
+// NAVIGATION
+// Menu Fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains("header__nav-link")) {
+    const link = e.target;
+    const siblings = link
+      .closest(".header__nav")
+      .querySelectorAll(".header__nav-link");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+// FOOTER
+
 // Updating the year in the footer
 
-const yearElement = document.querySelector('.year');
+const yearElement = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearElement.textContent = currentYear;
-    
-const btnNavEl = document.querySelector(".btn-mobile-nav"); 
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 const alllinks = document.querySelectorAll(".header__nav-link");
@@ -16,5 +40,5 @@ alllinks.forEach((link) => {
 });
 
 btnNavEl.addEventListener("click", () => {
-    headerEl.classList.toggle("nav-open");
+  headerEl.classList.toggle("nav-open");
 });
